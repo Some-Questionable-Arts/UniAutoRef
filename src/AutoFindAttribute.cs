54291adad8a5bb6fb46d0b6ee97e36ed73a6c593
TheQuizy <krutoyya109@gmail.com>
2026-08-13 01:41:32 +0300

    Update SRC for 0.9.9

--- src/AutoFindAttribute.cs
+++ src/AutoFindAttribute.cs
@@ -3,10 +3,42 @@ using UniAutoRef;
 
 namespace UniAutoRef
 {
+    /// <summary>
+    /// On-off debug. Recomended to enable.
+    /// </summary>
     public enum Debug : byte
     {
+        [Obsolete("Not recommended. Use Enable instead.")]
         Disable,
+
+        /// <summary>
+        /// Enables logging. Logging will not be included in the final build.
+        /// </summary>
         Enable,
+    }
+
+    /// <summary>
+    /// Where to find
+    /// </summary>
+    public enum FindIn : byte
+    {
+        /// <summary>
+        /// Finds in self. Uses: GetComponent<...>()
+        /// </summary>
+        Self,
+
+        /// <summary>
+        /// Finds in children. Uses: GetComponentInChildren<...>()
+        /// </summary>
+        Children,
+
+        /// <summary>
+        /// Finds in parent. Uses: GetComponentInParent<...>()
+        /// </summary>
+        Parent,
+
+        [Obsolete("Not recommended due to performance impact. Uses FindFirstObjectByType under the hood.")]
+        Scene,
     }
 }
 
@@ -14,9 +46,16 @@ namespace UniAutoRef
 public class AutoFindAttribute : Attribute 
 {
     public Debug IsDebug { get; }
+    public FindIn Find { get; }
 
-    public AutoFindAttribute(Debug isDebug = Debug.Enable)
+    /// <summary>
+    /// The attribute for auto-find components.
+    /// </summary>
+    /// <param name="isDebug">Debug mode.</param>
+    /// <param name="findIn">Where try find?</param>
+    public AutoFindAttribute(Debug isDebug = Debug.Enable, FindIn findIn = FindIn.Self)
     {
         IsDebug = isDebug;
+        Find = findIn;
     }
 }
