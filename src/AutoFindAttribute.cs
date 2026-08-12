25131c3bf4caaa55bc3b4c231c5b2245710561b0
TheQuizy <krutoyya109@gmail.com>
2026-08-12 14:24:29 +0300

    Create AutoFindAttribute.cs

---
+++ src/AutoFindAttribute.cs
@@ -0,0 +1,22 @@
+using System;
+using UniAutoRef;
+
+namespace UniAutoRef
+{
+    public enum Debug : byte
+    {
+        Disable,
+        Enable,
+    }
+}
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class AutoFindAttribute : Attribute 
+{
+    public Debug IsDebug { get; }
+
+    public AutoFindAttribute(Debug isDebug)
+    {
+        IsDebug = isDebug;
+    }
+}
