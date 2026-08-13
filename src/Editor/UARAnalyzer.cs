9fd067a3e1d6cf3eda317e4d567a894bf03dcc0c
TheQuizy <krutoyya109@gmail.com>
2026-08-13 23:29:59 +0300

    Update UARAnalyzer.cs

--- src/Editor/UARAnalyzer.cs
+++ src/Editor/UARAnalyzer.cs
@@ -5,13 +5,14 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-namespace UniAutoRef
+namespace UniAutoRef.Editor
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class UARAnalyzer : DiagnosticAnalyzer
     {
         public const string PartialDiagnosticId = "UAR010";
         public const string GeneratedAwakeDiagnosticId = "UAR011";
+        public const string GAwakeNotCalledDiagnosticId = "UAR012";
 
         private static readonly DiagnosticDescriptor PartialModifierRule = new(
             id: PartialDiagnosticId,
@@ -22,17 +23,26 @@ namespace UniAutoRef
             isEnabledByDefault: true
         );
 
-        private static readonly DiagnosticDescriptor GeneratedModifierRule = new(
+        private static readonly DiagnosticDescriptor GeneratedAwakeModifierRule = new(
             id: GeneratedAwakeDiagnosticId,
             title: "Class must have method \"GeneratedAwake\"",
             messageFormat: "Class '{0}' need to have \"GeneratedAwake\" method for using \"[AutoFind]\"",
             category: "Usage",
             defaultSeverity: DiagnosticSeverity.Error,
             isEnabledByDefault: true
-            );
+        );
+
+        private static readonly DiagnosticDescriptor GAwakeNotCalledModifierRule = new(
+            id: GAwakeNotCalledDiagnosticId,
+            title: "Method \"GeneratedAwake\" need to be called (in \"Awake\")",
+            messageFormat: "Class `{0}` need to call method \"GeneratedAwake\" (in \"Awake\")",
+            category: "Usage",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true
+        );
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
-            ImmutableArray.Create(PartialModifierRule, GeneratedModifierRule);
+            ImmutableArray.Create(PartialModifierRule, GeneratedAwakeModifierRule, GAwakeNotCalledModifierRule);
 
         public override void Initialize(AnalysisContext context)
         {
@@ -80,12 +90,40 @@ namespace UniAutoRef
             if (!hasGeneratedAwake)
             {
                 var generatedAwakeDiagnostic = Diagnostic.Create(
-                    GeneratedModifierRule,
+                    GeneratedAwakeModifierRule,
                     classDeclaration.Identifier.GetLocation(),
                     classDeclaration.Identifier.Text
                 );
                 context.ReportDiagnostic(generatedAwakeDiagnostic);
             }
+
+            var methodInvocations = classDeclaration.DescendantNodes()
+                .OfType<InvocationExpressionSyntax>();
+
+            bool isMethodCalled = methodInvocations.Any(invocation =>
+            {
+                if (invocation.Expression is IdentifierNameSyntax identifierName)
+                {
+                    return identifierName.Identifier.Text == "GeneratedAwake";
+                }
+                else if (invocation.Expression is MemberAccessExpressionSyntax memberAccess)
+                {
+                    return memberAccess.Name.Identifier.Text == "GeneratedAwake";
+                }
+
+                return false;
+            });
+
+            if (!isMethodCalled)
+            {
+                var gAwakeNotCalledDiagnostic = Diagnostic.Create(
+                    GAwakeNotCalledModifierRule,
+                    classDeclaration.Identifier.GetLocation(),
+                    classDeclaration.Identifier.Text
+                );
+                context.ReportDiagnostic(gAwakeNotCalledDiagnostic);
+            }
+
         }
 
 
